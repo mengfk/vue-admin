9a77e95b1290f7ccb4f1979724d92f560e4e2e6c
孟凡可 <898668957@qq.com>
2020-07-06 15:24:05 +0800

    构建项目、配置全局样式

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+      <h3>LOGIN红火火恍恍惚惚</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style lang='scss' scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
